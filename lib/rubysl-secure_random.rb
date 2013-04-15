0544520e89c011d305ac35b4ef8aa16a30b97ad7
Brian Shirai <brixen@gmail.com>
2013-04-14 17:14:52 -0700

    Created gem.

---
+++ lib/rubysl-secure_random.rb
@@ -0,0 +1,7 @@
+require "rubysl-secure_random/version"
+
+module RubySL
+  module SecureRandom
+    # Your code goes here...
+  end
+end
---
+++ lib/rubysl-secure_random/version.rb
@@ -0,0 +1,5 @@
+module RubySL
+  module SecureRandom
+    VERSION = "0.0.1"
+  end
+end
